35452ffdff847a91ed2c3c903a7a1af4e3b3fafc
David <david_ortiz87@live.com.ar>
2022-06-22 11:24:24 -0300

    clean assets logo & acm js

--- public/assets/js/acm/loged.js
+++
@@ -1,6 +0,0 @@
-// logout function 
-document.getElementById('logoutbtn').addEventListener('click',()=>{
-    confirmation('Seguro que quieres cerrar tu sesión?', ()=>{ 
-        document.location.href=`${URL}/acm/logout/`
-    })
-})
